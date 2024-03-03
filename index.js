9195eeb41e11b10a576bb6b0b70a83e9f5e875c9
Rishabh Verma <87109274+rishabhv2003@users.noreply.github.com>
2024-03-03 18:33:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'index.html'));
+});
+
+app.get('/room/:r_id', (req, res) => {
+    res.sendFile(join(__dirname, 'room.html'));
+})
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+
+server.listen(3000, () => {
+    console.log('server running at http://localhost:3000');
+});
